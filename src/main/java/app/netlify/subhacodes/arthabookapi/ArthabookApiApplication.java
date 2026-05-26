1c712b146f420fdcc29bac9ac5b09ae07fb0a3c3
Subhadip Dutta <subhadip3105@gmail.com>
2026-05-26 23:26:19 +0530

    initial setup

---
+++ src/main/java/app/netlify/subhacodes/arthabookapi/ArthabookApiApplication.java
@@ -0,0 +1,13 @@
+package app.netlify.subhacodes.arthabookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthabookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArthabookApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/netlify/subhacodes/arthabookapi/ArthabookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.netlify.subhacodes.arthabookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthabookApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
